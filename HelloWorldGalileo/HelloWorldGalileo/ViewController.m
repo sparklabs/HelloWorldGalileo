c88c38fa9b8daf7891ca1721f5adeafbeea01550
Wojtek Sczygiol <wojtek@konvulsator.de>
2014-03-20 10:26:49 -0700

    modified the example code to highlight some bugs/problems with the Galileo SDK

--- HelloWorldGalileo/HelloWorldGalileo/ViewController.m
+++ HelloWorldGalileo/HelloWorldGalileo/ViewController.m
@@ -5,9 +5,20 @@
 #import "ViewController.h"
 #import <GalileoControl/GalileoControl.h>
 
-@interface ViewController () <GCGalileoDelegate>
 
+// note: observed problems:
+
+//  1) When incrementTargetPosition... is called with waitUntilStationary: set to YES while another incrementTargetPosition call is still in progress,
+//     the galileo transitions into an unrecoverable error state where it is no longer able to read byte from the bluetooth connection. :(
+
+//  2) Sometimes the completionBlock for incrementTargetPosition:... is being called before the movement actually finishes.
+
+//  3) overflow error when rotationg around 360 degs
+
+
+@interface ViewController () <GCGalileoDelegate>
 @end
+
 
 @implementation ViewController
 
@@ -18,6 +29,7 @@
     
     // Start waiting for Galileo to connect
     [self disableUI];
+    [GCGalileo sharedGalileo].logLevel = GCLogLevelWarning;
     [GCGalileo sharedGalileo].delegate = self;
     [[GCGalileo sharedGalileo] waitForConnection];
 }
@@ -55,63 +67,104 @@
 
 - (void) disableUI
 {
-    [self.view setUserInteractionEnabled:NO];
+    // note: not blocking the UI on purpose to stress test concurrent commands
+    
+//    [self.view setUserInteractionEnabled:NO];
 }
 
 #pragma mark -
 #pragma mark Button handlers
 
+-(BOOL)_waitFlag {
+    // note: set this to YES to trigger problem 1)
+    return NO;
+}
+
+-(dispatch_queue_t)_dispatchQ {
+    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//    return dispatch_get_main_queue();
+}
+
+
 - (IBAction)panClockwise:(id)sender {
+    NSLog(@"panClockwise:");
+
     [self disableUI];
+    NSDate *now = [NSDate date];
     void (^completionBlock) (BOOL) = ^(BOOL wasCommandPreempted)
     {
-        if (!wasCommandPreempted) [self controlDidReachTargetPosition];
+        [self controlDidReachTargetPosition:wasCommandPreempted commandIssuedDate:now];
     };
-    [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan] incrementTargetPosition:90.0
-                                                                                    completionBlock:completionBlock waitUntilStationary:NO];
+    
+    dispatch_async([self _dispatchQ], ^{
+        [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan] incrementTargetPosition:359.9 completionBlock:completionBlock waitUntilStationary:[self _waitFlag]];
+    });
 }
 
 - (IBAction)panAnticlockwise:(id)sender {
+    NSLog(@"panAnticlockwise:");
+
     [self disableUI];
+    NSDate *now = [NSDate date];
     void (^completionBlock) (BOOL) = ^(BOOL wasCommandPreempted)
     {
-        if (!wasCommandPreempted) [self controlDidReachTargetPosition];
+        [self controlDidReachTargetPosition:wasCommandPreempted commandIssuedDate:now];
     };
-    [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan] incrementTargetPosition:-90.0
-                                                                                    completionBlock:completionBlock
-                                                                                waitUntilStationary:NO];
+    
+    dispatch_async([self _dispatchQ], ^{
+        [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan] incrementTargetPosition:-359.9 completionBlock:completionBlock waitUntilStationary:[self _waitFlag]];
+    });
 }
 
 - (IBAction)tiltClockwise:(id)sender {
+    NSLog(@"tiltClockwise:");
+    
     [self disableUI];
+    NSDate *now = [NSDate date];
     void (^completionBlock) (BOOL) = ^(BOOL wasCommandPreempted)
     {
-        if (!wasCommandPreempted) [self controlDidReachTargetPosition];
+        [self controlDidReachTargetPosition:wasCommandPreempted commandIssuedDate:now];
     };
-    [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisTilt] incrementTargetPosition:90
-                                                                                     completionBlock:completionBlock
-                                                                                 waitUntilStationary:NO];
+    
+    dispatch_async([self _dispatchQ], ^{
+        [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisTilt] incrementTargetPosition:359.9 completionBlock:completionBlock waitUntilStationary:[self _waitFlag]];
+    });
 }
 
 - (IBAction)tiltAnticlockwise:(id)sender {
+    NSLog(@"tiltAnticlockwise:");
+
     [self disableUI];
+    NSDate *now = [NSDate date];
     void (^completionBlock) (BOOL) = ^(BOOL wasCommandPreempted)
     {
-        if (!wasCommandPreempted) [self controlDidReachTargetPosition];
+        [self controlDidReachTargetPosition:wasCommandPreempted commandIssuedDate:now];
     };
-    [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisTilt] incrementTargetPosition:-90
-                                                                                     completionBlock:completionBlock
-                                                                                 waitUntilStationary:NO];
+    
+    dispatch_async([self _dispatchQ], ^{
+        [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisTilt] incrementTargetPosition:-359.9 completionBlock:completionBlock waitUntilStationary:[self _waitFlag]];
+    });
 }
 
 
 #pragma mark -
 #pragma mark PositionControl delegate
 
-- (void) controlDidReachTargetPosition
+- (void) controlDidReachTargetPosition:(BOOL)wasCommandPreempted commandIssuedDate:(NSDate*)commandIssuedDate
 {
-    // Re-enable the UI now that the target has been reached, assuming we are still connected to Galileo
-    if ([[GCGalileo sharedGalileo] isConnected]) [self enableUI];
+    NSDate *now = [NSDate date];
+    NSTimeInterval commandExecTime = [now timeIntervalSinceDate:commandIssuedDate];
+    NSLog(@"controlDidReachTargetPosition: wasCommandPreempted=?%i  commandExecTime=%f", wasCommandPreempted, commandExecTime);
+    
+    if (wasCommandPreempted == NO) {
+        // Re-enable the UI now that the target has been reached, assuming we are still connected to Galileo
+        if ([[GCGalileo sharedGalileo] isConnected]) {
+            [self enableUI];
+        }
+    } else {
+        
+    }
+    
 }
 
 
